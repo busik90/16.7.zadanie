2248074c59b260375d1f82f8fe602d1f7cb69d11
Pawel Bus <pawelbus.dev@outlook.com>
2018-07-24 19:19:56 +0200

    Remove unnecessary constructor from Todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,9 +2,6 @@ import React from 'react';
 import style from './css/Todo.css';
 
 class Todo extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
   componentDidMount = () => {
     setTimeout(() => this.props.animateTodoList('add', this.props.todoItem.id), 200);
